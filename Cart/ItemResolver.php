0ffd43435747ca51406146f772850cb653fd171b
Ivan Herak <ivan@netgen.hr>
2014-08-21 16:42:34 +0200

    Added allowed content types and modified setting the price

--- Cart/ItemResolver.php
+++ Cart/ItemResolver.php
@@ -10,10 +10,13 @@ use Netgen\EzSyliusBundle\Entity\EzProduct;
 class ItemResolver implements ItemResolverInterface
 {
     private $repository;
+    private $allowed_content_types;
+    //private $xmlToHtml5Converter;
 
-    public function __construct($repository)
+    public function __construct($repository, $allowed_content_types )
     {
         $this->repository = $repository;
+        $this->allowed_content_types = $allowed_content_types;
     }
 
     public function resolve(CartItemInterface $item, $request)
@@ -22,7 +25,7 @@ class ItemResolver implements ItemResolverInterface
 
         try {
             $product = new EzProduct();
-            $product->setEzContentAsProduct($productId, $this->repository);
+            $product->setEzContentAsProduct($productId, $this->repository, $this->allowed_content_types);
 
         } catch (\Exception $e) {
             throw new ItemResolvingException('Requested product was not found');
@@ -30,7 +33,8 @@ class ItemResolver implements ItemResolverInterface
 
         // Assign the product to the item and define the unit price.
         $item->setProduct( $product );
-        $item->setUnitPrice("222");
+        //$item->setUnitPrice("222");
+        $item->setUnitPrice($product->getPrice());
 
         // Everything went fine, return the item.
         return $item;
