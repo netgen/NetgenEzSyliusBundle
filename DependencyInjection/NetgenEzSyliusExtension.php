ba7b39be2413150515395171afd5468c0865f477
Edi Modrić <edi@netgen.hr>
2015-04-10 11:39:41 +0200

    Parametrize Sylius path prefix

--- DependencyInjection/NetgenEzSyliusExtension.php
+++ DependencyInjection/NetgenEzSyliusExtension.php
@@ -23,6 +23,7 @@ class NetgenEzSyliusExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('parameters.yml');
         $loader->load('services.yml');
         $loader->load('override.yml');
         $loader->load('fieldtypes.yml');
