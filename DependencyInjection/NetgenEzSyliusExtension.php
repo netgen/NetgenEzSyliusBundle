81a605eb669f2c0d596ff206cba052a6ce42f5ed
Edi Modrić <edi@netgen.hr>
2015-04-10 12:10:28 +0200

    Revert "Parametrize Sylius path prefix"
    This reverts commit ba7b39be2413150515395171afd5468c0865f477.

--- DependencyInjection/NetgenEzSyliusExtension.php
+++ DependencyInjection/NetgenEzSyliusExtension.php
@@ -23,7 +23,6 @@ class NetgenEzSyliusExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('parameters.yml');
         $loader->load('services.yml');
         $loader->load('override.yml');
         $loader->load('fieldtypes.yml');
