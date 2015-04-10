f92b12fb4989f8b7030075cabc9064ad776c99b2
Edi Modrić <edi@netgen.hr>
2015-04-10 15:44:40 +0200

    Remove ezsylius.yml, should be overriden if need arises

--- DependencyInjection/NetgenEzSyliusExtension.php
+++ DependencyInjection/NetgenEzSyliusExtension.php
@@ -27,7 +27,5 @@ class NetgenEzSyliusExtension extends Extension
         $loader->load('override.yml');
         $loader->load('fieldtypes.yml');
         $loader->load('storage_engines.yml');
-        $loader->load('ezsylius.yml');
-
     }
 }
