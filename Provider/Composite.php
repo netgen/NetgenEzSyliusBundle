611884add02971678ff3f0c03b2b1517e9b56f0a
Edi Modrić <edi@netgen.hr>
2016-09-12 10:56:02 +0200

    Composite user provider

---
+++ Provider/Composite.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Netgen\Bundle\EzSyliusBundle\Provider;
+
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Sylius\Bundle\UserBundle\Provider\UserProviderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class Composite implements UserProviderInterface
+{
+    /**
+     * @var \Sylius\Bundle\UserBundle\Provider\UserProviderInterface[]
+     */
+    protected $innerProviders = array();
+
+    /**
+     * Constructor.
+     *
+     * @param \Sylius\Bundle\UserBundle\Provider\UserProviderInterface[] $innerProviders
+     */
+    public function __construct(array $innerProviders)
+    {
+        $this->innerProviders = $innerProviders;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function loadUserByUsername($usernameOrEmail)
+    {
+        $exception = null;
+
+        foreach ($this->innerProviders as $provider) {
+            try {
+                return $provider->loadUserByUsername($usernameOrEmail);
+            } catch (UsernameNotFoundException $e) {
+                $exception = $e;
+            }
+        }
+
+        throw $exception;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        $exception = null;
+
+        foreach ($this->innerProviders as $provider) {
+            try {
+                return $provider->refreshUser($user);
+            } catch (UnsupportedUserException $e) {
+                $exception = $e;
+            } catch (UsernameNotFoundException $e) {
+                $exception = $e;
+            }
+        }
+
+        throw $exception;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supportsClass($class)
+    {
+        foreach ($this->innerProviders as $provider) {
+            if ($provider->supportsClass($class)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
