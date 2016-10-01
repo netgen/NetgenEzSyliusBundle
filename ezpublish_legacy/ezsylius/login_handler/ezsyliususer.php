cbd8a8b95da7baa785dbd69054071248de2f7ac0
crevillo <crevillo@gmail.com>
2016-10-01 20:27:17 +0200

    removed todo comment

--- ezpublish_legacy/ezsylius/login_handler/ezsyliususer.php
+++ ezpublish_legacy/ezsylius/login_handler/ezsyliususer.php
@@ -6,9 +6,6 @@ class eZSyliusUser extends eZUser
      * Checks if there's an admin sylius user having login entered by the user.
      * If so, it checks password entered is correct and if that is the case,
      * it returns the legacy user.
-     *
-     * @todo deal with the case where the name/email can be in sylius database
-     *    but is not connected to any ez user.
      */
     public static function loginUser($login, $password, $authenticationMatch = false)
     {
