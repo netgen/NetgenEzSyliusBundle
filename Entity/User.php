9f2098643806fbf5cf14455495d1b99031d524ae
Edi Modrić <edi.modric@gmail.com>
2017-05-08 16:26:07 +0200

    Add support for eZ UserReference in User object
    Ref: https://github.com/ezsystems/ezpublish-kernel/pull/1965

--- Entity/User.php
+++ Entity/User.php
@@ -3,20 +3,51 @@
 namespace Netgen\Bundle\EzSyliusBundle\Entity;
 
 use eZ\Publish\API\Repository\Values\User\User as APIUser;
+use eZ\Publish\Core\Repository\Values\User\UserReference;
 use Symfony\Component\Security\Core\User\UserInterface as BaseUserInterface;
+use LogicException;
 
 trait User
 {
+    /**
+     * @var \eZ\Publish\API\Repository\Values\User\UserReference
+     */
+    protected $reference;
+
     /**
      * @var \eZ\Publish\API\Repository\Values\User\User
      */
     protected $apiUser;
 
     /**
+     * @return \eZ\Publish\API\Repository\Values\User\UserReference
+     */
+    public function getAPIUserReference()
+    {
+        if ($this->reference === null && class_exists(UserReference::class)) {
+            $this->reference = new UserReference(
+                $this->apiUser instanceof APIUser ?
+                    $this->apiUser->id :
+                    null
+            );
+        }
+
+        return $this->reference;
+    }
+
+    /**
+     * @throws \LogicException If api user has not been refreshed yet by UserProvider after being unserialized from session.
+     *
      * @return \eZ\Publish\API\Repository\Values\User\User
      */
     public function getAPIUser()
     {
+        if (!$this->apiUser instanceof APIUser) {
+            throw new LogicException(
+                'Attempt to get APIUser before it has been set by UserProvider, APIUser is not serialized to session'
+            );
+        }
+
         return $this->apiUser;
     }
 
